0660581e0f84ec79c7a2bc47e8c03e237d6ab915
krimz-00 <krimzabdulkarim@gmail.com>
2023-09-13 15:09:00 +0100

    Added new funcions

--- code.c
+++ code.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 struct Node {
   int data;
@@ -7,43 +8,80 @@ struct Node {
 }*head = 0;
 
 void initialiseLinkedList(int sz);
-void displayCatalogue();
-void display(struct Node *ll);
-void rDisplay(struct Node *ll);
+void printCatalogue();
+void print(struct Node *ll);
+void rPrint(struct Node *ll);
+void countNodes(struct Node *ll);
+int rCountNodes(struct Node *ll);
+void sumNodes(struct Node *ll);
+int rSumNodes(struct Node *ll);
+void max(struct Node *ll);
+void min(struct Node *ll);
+void search(struct Node *ll, int x);
+void insert(struct Node *ll, int pos, int x);
+void insertLast(struct Node *ll, int x);
+bool checkSort(struct Node *ll);
+void insertSort(struct Node *ll, int x);
 void delete(struct Node *ll, int tmp);
 
-//some extra comment
 
 int main() {
   // struct Node *head = 0;
   int sz;
   int op;
   int tmp;
-  printf("Hello World!\nEnter the number of nodes you want inside the linked list: ");
+  printf("Enter the number of nodes you want inside the linked list: ");
   scanf("%d", &sz);
   initialiseLinkedList(sz);
   printf("Browse the list of opeations below.\n");
-  displayCatalogue();
+  printCatalogue();
   printf("Enter the number of operation you desire from the above list: ");
   scanf("%d", &op);
   switch (op) {
     case 0:
-      displayCatalogue();
+      printCatalogue();
       break;
     case 1:
-      display(head);
+      print(head);
       break;
     case 2:
-      rDisplay(head);
+      rPrint(head);
       break;
+    // case 3:
+    //   printf("Enter the position of the node that you want to delete: ");
+    //   scanf("%d", &tmp);
+    //   delete(head, tmp);
+    //   print(head);
+    //   break;
     case 3:
-      printf("Enter the position of the node that you want to delete: ");
-      scanf("%d", &tmp);
-      delete(head, tmp);
-      display(head);
+      countNodes(head);
       break;
+    case 4:
+      printf("The number of nodes recursively is: %d\n", rCountNodes(head));
+      break;
+    case 5:
+      sumNodes(head);
+      break;
+    case 6:
+      printf("The sum of nodes recursively is: %d\n", rSumNodes(head));
+      break;
+    case 7:
+      max(head);
+      print(head);
+      break;
+    case 8:
+      min(head);
+      sumNodes(head);
+      break;
+    case 9:
+      printf("Enter the number you are looking for: ");
+      scanf("%d", &tmp);
+      search(head, tmp);
+      print(head);
+      break;
+    
   }
-  // display(head);
+  // print(head);
 }
 
 void initialiseLinkedList(int sz) {
@@ -66,16 +104,18 @@ void initialiseLinkedList(int sz) {
   }
 }
 
-void displayCatalogue() {
+void printCatalogue() {
   printf("~~List of Operations~~\n");
-  printf("0: Display Catalogue Operation.\n");
-  printf("1: Iterative Display Operation.\n");
-  printf("2: Recursive Display Operation.\n");
-  printf("3: Delete Operation.\n");
+  printf("0: print Catalogue Operation.\n");
+  printf("1: Iterative print Operation.\n");
+  printf("2: Recursive print Operation.\n");
+  printf("3: Count Nodes Operation.\n");
+
+  // printf("3: Delete Operation.\n");
 
 }
 
-void display(struct Node *ll) {
+void print(struct Node *ll) {
   printf("Printing the linked list nodes: ");
   while (ll != 0) {
     printf("%d ", ll->data);
@@ -84,7 +124,7 @@ void display(struct Node *ll) {
   printf("\n");
 }
 
-void rDisplay(struct Node *ll) {
+void rPrint(struct Node *ll) {
   static int once = 1, anotherOnce = 1;
   if (once == 1) {
     printf("Printing the linked list nodes (recursively): ");
@@ -92,7 +132,7 @@ void rDisplay(struct Node *ll) {
   }
   if (ll != 0) {
     printf("%d ", ll->data);
-    rDisplay(ll->link);
+    rPrint(ll->link);
   }
   if (anotherOnce == 1) {
     printf("\n");
@@ -100,10 +140,75 @@ void rDisplay(struct Node *ll) {
   }
 }
 
+void countNodes(struct Node *ll) {
+  int count = 0;
+  struct Node *p = ll;
+  while (p != 0) {
+    count++;
+    p = p->link;
+  }
+  printf("The number of nodes present in the linked list is: %d\n", count);
+}
+
+int rCountNodes(struct Node *ll) {
+  if (ll == 0)
+    return 0;
+  else
+    return rCountNodes(ll->link) + 1;
+}
+
+void sumNodes(struct Node *ll) {
+  struct Node *p = ll;
+  int sum = 0;
+  while (p != 0) {
+    sum += p->data;
+    p = p->link;
+  }
+  printf("The sum of the nodes is: %d\n", sum);
+}
+
+int rSumNodes(struct Node *ll) {
+  if (ll == 0)
+    return 0;
+  else
+    return rSumNodes(ll->link) + ll->data;
+}
+
+void max(struct Node *p) {
+  int mx = p->data;
+  while (p != 0) {
+    if (mx < p->data)
+      mx = p->data;
+    p = p->link;
+  }
+  printf("The maximum number from the linked list is: %d\n", mx);
+}
+
+void min(struct Node *p) {
+  int mn = p->data;
+  while (p != 0) {
+    if (p->data < mn)
+      mn = p->data;
+    p = p->link;
+  }
+  printf("The minimum number from the linked list is: %d\n", mn);
+}
+
+void search(struct Node *p, int x) {
+  while (p != 0) {
+    if (p->data == x) {
+      printf("Found!\n");
+      return;
+    }
+    p = p->link;
+  }
+  printf("The number is not found anywhere!\n");
+}
+
 void delete(struct Node *ll, int pos) {
-  struct Node *p = ll, *q = 0;
-  if (pos == 0) {
-    ll = ll->link;
+  struct Node *p = head, *q = 0;
+  if (pos == 1) {
+    head = head->link;
     free(p);
   }
   else {
